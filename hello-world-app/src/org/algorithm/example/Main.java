b100caceac5af3c4a3d17a55b57f7166714aff18
Zdeněk Gold <algoritmuj@gmail.com>
2019-09-20 19:52:13 +0200

    Hello World (první aplikace) #1

---
+++ hello-world-app/src/org/algorithm/example/Main.java
@@ -0,0 +1,12 @@
+package org.algorithm.example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// vytiskneme text do konzole
+		System.out.println("HELLO WORLD!");
+
+		// v tomto místì jsou už vykonány všechny pøíkazy
+		// po ukonèení této metody dojde také k ukonèení celého programu
+	}
+}
